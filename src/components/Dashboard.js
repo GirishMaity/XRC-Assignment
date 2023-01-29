408efdc055b90f9888555fe9e4184bd2a13ff690
Girish Maity <girishmaity164@gmail.com>
2023-01-30 00:05:24 +0530

    Added Welcome toast

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -3,6 +3,9 @@ import Navbar from "./Navbar";
 import SideBar from "./SideBar";
 import styled from "styled-components";
 import Cards4 from "./Cards4";
+import { ToastContainer, toast } from "react-toastify";
+
+import "react-toastify/dist/ReactToastify.css";
 
 const Container = styled.div`
   background: #2c2f33;
@@ -11,8 +14,31 @@ const Container = styled.div`
 `;
 
 const Dashboard = () => {
+  const data = localStorage.getItem("data");
+  toast.success(`Welcome! ${data}`, {
+    position: "top-right",
+    autoClose: 5000,
+    hideProgressBar: true,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "dark",
+  });
   return (
     <Container>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
       <Navbar />
       <SideBar />
       <Cards4 />
